6b946c7e26ce2beca8194418b2affa77e26d1e1c
Dan Rue <dan.rue@linaro.org>
2019-09-12 13:40:07 +0100

    Support multiple branches
    Find all individual test results since a given date. Note that it bails
    out after 10 results per build at the moment.
    
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- lkft/lkft.py
+++ lkft/lkft.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-import squad_client  # noqa: E402
+import argparse
+import datetime
+import sys
+import squad_client
 
 
 def transform_lkft_to_kci(test, build_version):
@@ -40,13 +43,51 @@ def transform_lkft_to_kci(test, build_version):
         "vcs_commit": build_version,
     }
 
+def valid_date_type(arg_date_str):
+    """custom argparse *date* type for user dates values given from the command line"""
+    try:
+        return datetime.datetime.strptime(arg_date_str, "%Y-%m-%d")
+    except:
+        print(
+            "Given Date ({0}) not valid! Expected format, YYYY-MM-DD!".format(
+                arg_date_str
+            )
+        )
+        sys.exit(1)
+
 
 if __name__ == "__main__":
 
-    build_url = "https://qa-reports.linaro.org/api/builds/22006/"  # v5.3-rc8
-    build = squad_client.Build(build_url)
-    build_version = build.build["version"]
+    parser = argparse.ArgumentParser(
+        description="Generate report of branches tested recently"
+    )
+    parser.add_argument(
+        dest="date",
+        type=valid_date_type,
+        help='Report on builds that occured since date given (inclusive) "YYYY-MM-DD"',
+    )
+    args = parser.parse_args()
+    date = args.date
 
-    for testrun in squad_client.get_objects(build.build.get("testruns")):
-        for test in squad_client.get_objects(testrun.get("tests")):
-            print(transform_lkft_to_kci(test, build_version))
+    branches = squad_client.get_projects_by_branch()
+    for branch, branch_url in branches.items():
+        builds_url = branch_url+"builds/"
+        for build in squad_client.Builds(builds_url):
+            if date > datetime.datetime.strptime(
+                build["datetime"], "%Y-%m-%dT%H:%M:%S.%fZ"
+            ):
+                break
+            build_version = build["version"]
+            for testrun in squad_client.get_objects(build.get("testruns")):
+                for index, test in enumerate(squad_client.get_objects(testrun.get("tests"))):
+                    print(transform_lkft_to_kci(test, build_version))
+                    if index > 10:
+                        print("Bailing out after 10 test results")
+                        break
+
+
+
+    #build_url = "https://qa-aep rts.linaro.org/api/builds/22006/"  # v5.3-rc8
+    #build = squad_client.Build(build_url)
+    #build_version = build.build["version"]
+
