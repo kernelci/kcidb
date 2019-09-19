a6a9b8d3f30af3caaef552c513428914394fbadb
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2019-09-19 13:10:37 +0200

    Add revision and build tables

--- kcidb/io_schema.py
+++ kcidb/io_schema.py
@@ -2,22 +2,95 @@
 
 import jsonschema
 
-# JSON schema for a test execution
-JSON_TEST = {
-    "title": "test",
-    "description": "A test execution",
+# JSON schema for a code revision
+JSON_REVISION = {
+    "title": "revision",
+    "description": "A revision of the tested code",
     "type": "object",
     "properties": {
-        "build_path": {
+        "origin": {
             "type": "string",
             "description":
-                "Dot-separated build tree path of the build being tested",
-            "pattern": "^[.a-zA-Z0-9_]*$"
+                "The name of the CI system which submitted the revision",
+            "pattern": "^[a-z0-9_]*$"
+        },
+        "origin_id": {
+            "type": "string",
+            "description": "Origin-unique revision ID",
+        },
+    },
+    "required": [
+        "origin",
+        "origin_id",
+    ],
+}
+
+# JSON schema for a build of a revision
+JSON_BUILD = {
+    "title": "build",
+    "description": "A build of a revision",
+    "type": "object",
+    "properties": {
+        "revision_origin": {
+            "type": "string",
+            "description":
+                "The name of the CI system which submitted the built revision",
+            "pattern": "^[a-z0-9_]*$"
+        },
+        "revision_origin_id": {
+            "type": "string",
+            "description": "Origin-unique ID of the built revision",
+        },
+        "origin": {
+            "type": "string",
+            "description":
+                "The name of the CI system which submitted the build",
+            "pattern": "^[a-z0-9_]*$"
+        },
+        "origin_id": {
+            "type": "string",
+            "description": "Origin-unique build ID",
+        },
+    },
+    "required": [
+        "revision_origin",
+        "revision_origin_id",
+        "origin",
+        "origin_id",
+    ],
+}
+
+# JSON schema for a test run on a build
+JSON_TEST = {
+    "title": "test",
+    "description": "A test run against a build",
+    "type": "object",
+    "properties": {
+        "build_origin": {
+            "type": "string",
+            "description":
+                "The name of the CI system which submitted the tested build",
+            "pattern": "^[a-z0-9_]*$"
+        },
+        "build_origin_id": {
+            "type": "string",
+            "description": "Origin-unique ID of the tested build",
+        },
+        "origin": {
+            "type": "string",
+            "description":
+                "The name of the CI system which submitted the test run",
+            "pattern": "^[a-z0-9_]*$"
+        },
+        "origin_id": {
+            "type": "string",
+            "description": "Origin-unique ID of the tested build",
         },
         "path": {
             "type": "string",
             "description":
-                "Dot-separated test tree path of the test being executed",
+                "Dot-separated canonical test tree path "
+                "of the test being executed",
             "pattern": "^[.a-zA-Z0-9_]*$"
         },
         "status": {
@@ -28,11 +101,13 @@ JSON_TEST = {
         "waived": {
             "type": "boolean",
             "description": "Waived flag",
-            "default": False,
-        }
+        },
     },
     "required": [
-        "build_path",
+        "build_origin",
+        "build_origin_id",
+        "origin",
+        "origin_id",
         "path",
     ],
 }
@@ -48,8 +123,18 @@ JSON = {
             "description": "Version of the schema the data complies to",
             "const": "1"
         },
+        "revision_list": {
+            "description": "List of code revisions",
+            "type": "array",
+            "items": JSON_REVISION,
+        },
+        "build_list": {
+            "description": "List of builds",
+            "type": "array",
+            "items": JSON_BUILD,
+        },
         "test_list": {
-            "description": "List of test executions",
+            "description": "List of test runs",
             "type": "array",
             "items": JSON_TEST,
         },
--- kcidb/submit_cmd.py
+++ kcidb/submit_cmd.py
@@ -1,10 +1,22 @@
 """kcidb-submit command-line executable"""
 
 import argparse
+import time
 import sys
 import json
 from google.cloud import bigquery
+from kcidb import db_schema
 from kcidb import io_schema
+
+
+def generate_id():
+    """
+    Generate a pseudo-unique increasing ID.
+
+    Returns:
+        A pseudo-unique integer ID.
+    """
+    return int(time.time()) * 1000000000 + time.time_ns()
 
 
 def main():
@@ -23,7 +35,15 @@ def main():
 
     client = bigquery.Client()
     dataset_ref = client.dataset(args.dataset)
-    if "test_list" in data:
-        table_ref = dataset_ref.table("tests")
-        job = client.load_table_from_json(data["test_list"], table_ref)
-        job.result()
+    for obj_name in ("revision", "build", "test"):
+        if obj_name + "_list" in data:
+            for obj in data[obj_name + "_list"]:
+                obj["id"] = generate_id()
+            table_name = obj_name + "s"
+            property_name = obj_name + "_list"
+            job_config = bigquery.job.LoadJobConfig(
+                autodetect=False, schema=db_schema.TABLE_MAP[table_name])
+            table_ref = dataset_ref.table(table_name)
+            job = client.load_table_from_json(data[property_name],
+                                              table_ref, job_config=job_config)
+            job.result()
