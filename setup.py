9d6a484f6a0b34f61951742614ed2cf6d8e3a16e
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-02-20 19:25:33 +0200

    Move version in setup.py past the latest tag
    Update version in setup.py to be past the latest tag.
    This should've been (and should be in the future) done in the next
    commit after the tag.
    
    Not only *that* was forgotten, but also updating the release number for
    the last tag.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kcidb",
-    version="1",
+    version="3",
     python_requires=">=3.6",
     author="kernelci.org",
     author_email="kernelci@groups.io",
