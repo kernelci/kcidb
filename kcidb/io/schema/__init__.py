62df36d7c653c55afb0797f930f86bbcf625d6b5
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-03-12 14:30:03 +0200

    io.schema: Add v2
    Add the v2 version of the I/O schema, identical to the first with the
    exception of the version number. This prepares for schema changes to be
    introduced.

--- kcidb/io/schema/__init__.py
+++ kcidb/io/schema/__init__.py
@@ -1,8 +1,11 @@
 """Kernel CI reporting I/O schema"""
 
 from kcidb.io.schema import v1
+from kcidb.io.schema import v2
 
 # Version 1
 V1 = v1.VERSION
+# Version 2
+V2 = v2.VERSION
 # Latest version of the schema
-LATEST = V1
+LATEST = V2
