136637bb518a713e38cdec9bef80c29d6996abe2
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-04-07 14:32:28 +0300

    Split kcidb_consume into kcidb_consume_new/loaded

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
 """Google Cloud Functions for Kernel CI reporting"""
 
 import os
-import sys
-import json
 import base64
 import smtplib
 import kcidb
 
+PROJECT_ID = os.environ["GCP_PROJECT"]
 DATASET = os.environ["KCIDB_DATASET"]
 SMTP_HOST = os.environ.get("KCIDB_SMTP_HOST", "")
 SMTP_TO_ADDRS = os.environ.get("KCIDB_SMTP_TO_ADDRS", None)
+MQ_LOADED_TOPIC = os.environ["KCIDB_MQ_LOADED_TOPIC"]
 
 DB_CLIENT = kcidb.db.Client(DATASET)
 SPOOL_CLIENT = kcidb.spool.Client()
+MQ_LOADED_PUBLISHER = kcidb.mq.Publisher(PROJECT_ID, MQ_LOADED_TOPIC)
 
 
 # pylint: disable=unused-argument
-def kcidb_consume(event, context):
+def kcidb_consume_new(event, context):
     """
-    Consume KCIDB data from a Pub Sub subscription
+    Consume new KCIDB data from a Pub Sub subscription
     """
     # Get new data
     io_new = kcidb.mq.Subscriber.decode_data(base64.b64decode(event["data"]))
-    print("RECEIVED")
-    json.dump(io_new, sys.stdout)
     # Store it in the database
     DB_CLIENT.load(io_new)
-    # Load the complement: the new data along with all the linked
+    # Forward the data to the "loaded" MQ topic
+    MQ_LOADED_PUBLISHER.publish(io_new)
+
+
+def kcidb_consume_loaded(event, context):
+    """
+    Consume loaded KCIDB data from a Pub Sub subscription
+    """
+    # Get loaded data
+    io_loaded = kcidb.mq.Subscriber.decode_data(
+        base64.b64decode(event["data"])
+    )
+    # Load the complement: the loaded data along with all the linked
     # parents and children
-    io_complement = DB_CLIENT.complement(io_new)
-    # Convert new and complemented data to object-oriented representation
-    oo_new = kcidb.oo.from_io(io_new)
+    io_complement = DB_CLIENT.complement(io_loaded)
+    # Convert loaded and complemented data to object-oriented representation
+    oo_loaded = kcidb.oo.from_io(io_loaded)
     oo_complement = kcidb.oo.from_io(io_complement)
     # Remove all objects with missing parents
     oo_rooted_complement = kcidb.oo.remove_orphans(oo_complement)
-    # Delist everything except new or modified objects, but keep references
-    oo_new_rooted_complement = kcidb.oo.apply_mask(oo_rooted_complement,
-                                                   oo_new)
-    print("NOTIFIED")
-    json.dump(kcidb.oo.to_io(oo_new_rooted_complement), sys.stdout)
+    # Delist everything except loaded or modified objects, but keep references
+    oo_loaded_rooted_complement = kcidb.oo.apply_mask(oo_rooted_complement,
+                                                      oo_loaded)
     # Spool notifications from subscriptions
-    for notification in kcidb.subscriptions.match(oo_new_rooted_complement):
+    for notification in kcidb.subscriptions.match(oo_loaded_rooted_complement):
+        print("NOTIFICATION ID:", notification.id)
         SPOOL_CLIENT.put(notification)
 
 
