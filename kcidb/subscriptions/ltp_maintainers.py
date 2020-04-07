976e297e90eb6c31524be884d024ac17e2f270cd
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-04-07 11:47:40 +0300

    subscriptions: Fix iteration in draft subscriptions

--- kcidb/subscriptions/ltp_maintainers.py
+++ kcidb/subscriptions/ltp_maintainers.py
@@ -6,8 +6,8 @@ from kcidb.misc import NotificationMessage as Message
 def match_revision(revision):
     """Match revisions of interest to LTP maintainers"""
     recipients = ["LTP Mailing List <ltp@lists.linux.it>"]
-    for build in revision.builds_:
-        for test in build.tests_:
+    for build in revision.builds_.values():
+        for test in build.tests_.values():
             if test.path == "ltp":
                 if test.status == "FAIL":
                     return (Message(recipients, "LTP failed"),)
