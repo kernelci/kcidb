eeb908ec599563db959f6ba96b6e13ad4b86e57f
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-04-07 11:47:40 +0300

    subscriptions: Fix iteration in draft subscriptions

--- kcidb/subscriptions/rdma.py
+++ kcidb/subscriptions/rdma.py
@@ -6,10 +6,10 @@ from kcidb.misc import NotificationMessage as Message
 def match_revision(revision):
     """Match revisions of interest to RDMA tree developers"""
     recipients = ["rdma-dev-team@redhat.com"]
-    for build in revision.builds_:
+    for build in revision.builds_.values():
         if not build.valid:
             return (Message(recipients, "Build failed"),)
-        for test in build.tests_:
+        for test in build.tests_.values():
             if test.status in ("FAIL", "ERROR"):
                 return (Message(recipients, "Tests failed"),)
     return ()
