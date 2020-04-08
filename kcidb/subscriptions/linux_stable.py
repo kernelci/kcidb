1da4798fead03a139cd55eaab4cf1d508d0ef518
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-04-08 20:44:17 +0300

    subscriptions: Replace rdma with linux-stable

---
+++ kcidb/subscriptions/linux_stable.py
@@ -0,0 +1,26 @@
+"""RDMA tree subscriptions"""
+
+from kcidb.misc import NotificationMessage as Message
+
+REPO_URL_BASE = "https://git.kernel.org/pub/scm/linux/kernel/git"
+REPO_URL_LIST = [
+    REPO_URL_BASE + suffix for suffix in [
+        "/stable/linux-stable-rc.git",
+        "/sashal/linux-stable.git",
+        "/stable/stable-queue.git",
+    ]
+]
+
+
+def match_revision(revision):
+    """Match revisions of interest to stable tree developers"""
+    recipients = ["Linux Stable maillist <stable@vger.kernel.org>"]
+    if revision.git_repository_url not in REPO_URL_LIST:
+        return ()
+    for build in revision.builds_.values():
+        if not build.valid:
+            return (Message(recipients, "Builds failed for "),)
+        for test in build.tests_.values():
+            if test.status in ("FAIL", "ERROR") and not test.waived:
+                return (Message(recipients, "Tests failed for "),)
+    return ()
