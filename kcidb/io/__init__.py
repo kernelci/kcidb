fa4b6323839f894fdf08d16c351c5a230eddcec4
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-04-17 20:04:05 +0300

    Implement and use kcidb.io.new()

--- kcidb/io/__init__.py
+++ kcidb/io/__init__.py
@@ -3,4 +3,17 @@
 from kcidb.io import schema
 
 # Silence flake8 "imported but unused" warning
-__all__ = ["schema"]
+__all__ = ["schema", "new"]
+
+
+def new():
+    """
+    Create an empty I/O data set.
+
+    Returns:
+        An empty I/O data set adhering to the latest schema version.
+    """
+    data = dict(version=dict(major=schema.LATEST.major,
+                             minor=schema.LATEST.minor))
+    assert schema.is_valid_latest(data)
+    return data
