8f7696909de866c8921425a83b18743cfe5c498f
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-04-22 14:46:09 +0300

    Add a test for executable entry points

---
+++ test_setup.py
@@ -0,0 +1,32 @@
+"""setup.py tests"""
+
+import re
+import sys
+import unittest
+from importlib import import_module
+from unittest.mock import patch
+
+
+class SetupTestCase(unittest.TestCase):
+    """setup.py test case"""
+
+    @patch('setuptools.setup')
+    def test_console_scripts(self, setuptools_setup):
+        """Check all entry points could be executed"""
+        import_module("setup")
+        console_scripts = \
+            setuptools_setup.call_args[1]['entry_points']['console_scripts']
+        for console_script in console_scripts:
+            match = re.fullmatch(
+                "([^=]+) *= *([a-zA-Z0-9._]+):([a-zA-Z0-9_]+)", console_script)
+            executable = match.group(1)
+            module = match.group(2)
+            function = match.group(3)
+            orig_argv = sys.argv
+            try:
+                sys.argv = [executable, "--help"]
+                with self.assertRaises(SystemExit) as context_manager:
+                    getattr(import_module(module), function)()
+                self.assertEqual(context_manager.exception.code, 0)
+            finally:
+                sys.argv = orig_argv
