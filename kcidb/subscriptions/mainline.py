56af2e6d1a477a1f791c186e048e3fb256fbcf74
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-04-22 19:31:52 +0300

    subscriptions: Add mainline

---
+++ kcidb/subscriptions/mainline.py
@@ -0,0 +1,20 @@
+"""Mainline tree subscriptions"""
+
+from kcidb.misc import NotificationMessage as Message
+
+REPO_URL = \
+    "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git"
+RECIPIENTS = ["Linux Kernel Mailing List <linux-kernel@vger.kernel.org>"]
+
+
+def match_revision(revision):
+    """Match revisions of interest to stable tree developers"""
+    if revision.git_repository_url != REPO_URL:
+        return ()
+    for build in revision.builds_.values():
+        if not build.valid:
+            return (Message(RECIPIENTS, "Builds failed for "),)
+        for test in build.tests_.values():
+            if test.status == "FAIL" and not test.waived:
+                return (Message(RECIPIENTS, "Tests failed for "),)
+    return ()
