5ee1bdaeba78cf2117917031916c5117b6f31a81
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-05-13 18:59:33 +0300

    Add a copy of v2 schema as initial v3 schema

--- kcidb/io/schema/__init__.py
+++ kcidb/io/schema/__init__.py
@@ -1,14 +1,15 @@
 """Kernel CI reporting I/O schema"""
 
-from kcidb.io.schema import v1
-from kcidb.io.schema import v2
+from kcidb.io.schema import v1, v2, v3
 
 # Version 1
 V1 = v1.VERSION
 # Version 2
 V2 = v2.VERSION
+# Version 3
+V3 = v3.VERSION
 # Latest version of the schema
-LATEST = V2
+LATEST = V3
 
 
 def validate(data):
