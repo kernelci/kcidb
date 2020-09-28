d054a0a40ae699051ebe83c3b58bc60780006829
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-09-28 15:33:30 +0300

    tests: Add main function tests

---
+++ kcidb/test_tests.py
@@ -0,0 +1,38 @@
+"""kcdib.tests module tests"""
+
+import textwrap
+import kcidb
+
+
+class KCIDBTestsMainFunctionsTestCase(kcidb.unittest.TestCase):
+    """Test case for main functions"""
+
+    def test_validate_main(self):
+        """Check kcidb-tests-validate works"""
+        self.assertExecutes('', "kcidb.tests.validate_main",
+                            status=1, stderr_re=".*ValidationError.*")
+        self.assertExecutes('test: "', "kcidb.tests.validate_main",
+                            status=1, stderr_re=".*ScannerError.*")
+        self.assertExecutes(textwrap.dedent("""
+                                te/st:
+                                    title: title
+                                    home: https://example.com
+                            """),
+                            "kcidb.tests.validate_main",
+                            status=1, stderr_re=".*ValidationError.*")
+        self.assertExecutes(textwrap.dedent("""
+                                test:
+                                    title: title
+                                    home: https://example.com
+                            """),
+                            "kcidb.tests.validate_main")
+        self.assertExecutes(textwrap.dedent("""
+                                test:
+                                    title: title
+                                    description: title
+                                    home: https://example.com
+                                    description: |
+                                        desc
+                                        ription
+                            """),
+                            "kcidb.tests.validate_main")
