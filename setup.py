f8f185af9a70a6a48c97ad225acb90f641adccf9
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-10-02 14:34:36 +0300

    Bump version number to v7.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kcidb",
-    version="7",
+    version="7.1",
     python_requires=">=3.6",
     author="kernelci.org",
     author_email="kernelci@groups.io",
