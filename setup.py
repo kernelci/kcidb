81a1bb79f0bd27c2e61a1b6a312be7ccfa799eb4
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-10-13 14:50:11 +0300

    Bump version number for next release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kcidb",
-    version="8",
+    version="9",
     python_requires=">=3.6",
     author="kernelci.org",
     author_email="kernelci@groups.io",
