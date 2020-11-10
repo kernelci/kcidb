9d9df05aaa25e0e5bc599a62dae1379031610e29
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2020-11-10 19:58:14 +0200

    tests: Check all I/O fields can be loaded to DB

--- kcidb/test_db.py
+++ kcidb/test_db.py
@@ -3,6 +3,7 @@
 import re
 import textwrap
 import json
+from unittest.mock import Mock, patch
 import kcidb_io
 import kcidb
 
@@ -225,3 +226,118 @@ class KCIDBDBMainFunctionsTestCase(kcidb.unittest.TestCase):
                             driver_source=driver_source,
                             stdout_re=re.escape(json.dumps(report_b) + "\n" +
                                                 json.dumps(report_a) + "\n"))
+
+
+class KCIDBDBClient(kcidb.unittest.TestCase):
+    """Test case for the Client class"""
+
+    def test_load(self):
+        """Check all possible I/O fields can be loaded"""
+        # Calm down, pylint: disable=no-self-use
+        io_data = {
+            **kcidb_io.new(),
+            "revisions": [
+                dict(
+                    id="ef2b753c38bc9c0d1eea84e29a6bb6f9e776d0e3",
+                    origin="origin",
+                    tree_name="mainline",
+                    git_repository_url="https://git.kernel.org/pub/scm/"
+                                       "linux/kernel/git/torvalds/linux.git",
+                    git_commit_hash="ef2b753c38bc9c0d1eea84e29a6bb6f9e776d0e3",
+                    git_commit_name="v5.8-rc7-98-g7dc6fd0f3b84",
+                    git_repository_branch="master",
+                    patch_mboxes=[
+                        dict(name="0001.patch",
+                             url="https://example.com/0001.patch"),
+                        dict(name="0002.patch",
+                             url="https://example.com/0002.patch"),
+                    ],
+                    message_id="5f9fccd2.1c69fb81.1b2f.6d9c@mx.google.com",
+                    description="v5.8",
+                    publishing_time="2020-08-14T23:08:06.967000+00:00",
+                    discovery_time="2020-08-14T23:08:06.967000+00:00",
+                    contacts=[
+                        "clang-built-linux@googlegroups.com",
+                        "stable@vger.kernel.org"
+                    ],
+                    log_url="https://example.com/revision.log",
+                    valid=True,
+                    misc=dict(
+                        foo="bar",
+                        baz=42
+                    ),
+                ),
+            ],
+            "builds": [
+                dict(
+                    revision_id="ef2b753c38bc9c0d1eea84e29a6bb6f9e776d0e3",
+                    id="origin:1",
+                    origin="origin",
+                    description="x86_64",
+                    start_time="2020-08-14T23:08:06.967000+00:00",
+                    duration=600,
+                    architecture="x86_64",
+                    command="make",
+                    compiler="gcc",
+                    input_files=[
+                        dict(name="foo.bar",
+                             url="https://example.com/foo.bar"),
+                        dict(name="bar.foo",
+                             url="https://example.com/bar.foo"),
+                    ],
+                    output_files=[
+                        dict(name="foo.bar",
+                             url="https://example.com/foo.bar"),
+                        dict(name="bar.foo",
+                             url="https://example.com/bar.foo"),
+                    ],
+                    config_name="fedora",
+                    config_url="https://example.com/fedora.config",
+                    log_url="https://example.com/build.log",
+                    valid=True,
+                    misc=dict(
+                        foo="bar",
+                        baz=42
+                    ),
+                ),
+            ],
+            "tests": [
+                dict(
+                    build_id="origin:1",
+                    id="origin:1",
+                    origin="origin",
+                    environment=dict(
+                        description="Lenovo x230",
+                        misc=dict(
+                            foo="bar",
+                            baz=42
+                        ),
+                    ),
+                    path="ltp",
+                    description="ltp on Lenovo x230",
+                    status="PASS",
+                    waived=False,
+                    start_time="2020-08-14T23:08:07.967000+00:00",
+                    duration=600,
+                    output_files=[
+                        dict(name="foo.bar",
+                             url="https://example.com/foo.bar"),
+                        dict(name="bar.foo",
+                             url="https://example.com/bar.foo"),
+                    ],
+                    misc=dict(
+                        foo="bar",
+                        baz=42
+                    ),
+                ),
+            ]
+        }
+        dataset = Mock()
+        dataset.labels = dict(version_major=io_data['version']['major'],
+                              version_minor=io_data['version']['minor'])
+        client = Mock()
+        client.get_dataset = Mock(return_value=dataset)
+        with patch("google.cloud.bigquery.Client", return_value=client), \
+             patch("google.cloud.bigquery.job.LoadJobConfig"):
+            client = kcidb.db.Client("dataset")
+            client.load(io_data)
