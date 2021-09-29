2044ade80bd0edcf6a0f11003d09dcbd9e49616e
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2021-09-29 13:26:45 +0300

    main: Do not debug-log the message itself

--- main.py
+++ main.py
@@ -218,8 +218,6 @@ def kcidb_spool_notifications(event, context):
             SPOOL_CLIENT.post(notification)
         else:
             LOGGER.info("DROPPING ID %s", notification.id)
-            LOGGER.debug("DROPPING MESSAGE:\n%s",
-                         notification.render().as_string())
 
 
 def kcidb_send_notification(data, context):
