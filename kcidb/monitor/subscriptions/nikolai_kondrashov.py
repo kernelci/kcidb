2ee7854eaef24484541af4188b960f9bbb649af0
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2021-10-04 17:13:46 +0300

    monitor: Subscribe Nikolai Kondrashov

---
+++ kcidb/monitor/subscriptions/nikolai_kondrashov.py
@@ -0,0 +1,19 @@
+"""Nikolai Kondrashov's subscription"""
+
+from kcidb.monitor.output import NotificationMessage as Message
+
+RECIPIENTS = ["Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>"]
+
+
+def match_revision(revision):
+    """Match revisions of interest to Nikolai Kondrashov"""
+    if revision.checkouts_valid is not None:
+        if not revision.checkouts_valid:
+            return (Message(RECIPIENTS, "Checkouts failed for "),)
+        if revision.builds_valid is not None:
+            if not revision.builds_valid:
+                return (Message(RECIPIENTS, "Builds failed for "),)
+            if revision.tests_root.status is not None:
+                if revision.tests_root.status not in ("PASS", "DONE", "SKIP"):
+                    return (Message(RECIPIENTS, "Tests failed for "),)
+    return ()
