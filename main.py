baacc1f61df0cd71be478d772591ed40dd5c2008
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2022-05-31 13:00:34 +0300

    main: Access Google Cloud on demand only

--- main.py
+++ main.py
@@ -18,11 +18,7 @@ kcidb.misc.logging_setup(
 LOGGER = logging.getLogger()
 
 # The subscriber object for the submission queue
-LOAD_QUEUE_SUBSCRIBER = kcidb.mq.IOSubscriber(
-    PROJECT_ID,
-    os.environ["KCIDB_LOAD_QUEUE_TOPIC"],
-    os.environ["KCIDB_LOAD_QUEUE_SUBSCRIPTION"]
-)
+_LOAD_QUEUE_SUBSCRIBER = None
 # Maximum number of messages loaded from the submission queue in one go
 LOAD_QUEUE_MSG_MAX = int(os.environ["KCIDB_LOAD_QUEUE_MSG_MAX"])
 # Maximum number of objects loaded from the submission queue in one go
@@ -30,9 +26,6 @@ LOAD_QUEUE_OBJ_MAX = int(os.environ["KCIDB_LOAD_QUEUE_OBJ_MAX"])
 # Maximum time for pulling maximum amount of submissions from the queue
 LOAD_QUEUE_TIMEOUT_SEC = float(os.environ["KCIDB_LOAD_QUEUE_TIMEOUT_SEC"])
 
-# String specifying the database to access,
-# i.e. the kcidb.db.Client.__init__() argument.
-DATABASE = os.environ["KCIDB_DATABASE"]
 # Minimum time between loading submissions into the database
 DATABASE_LOAD_PERIOD = datetime.timedelta(
     seconds=int(os.environ["KCIDB_DATABASE_LOAD_PERIOD_SEC"])
@@ -42,19 +35,14 @@ DATABASE_LOAD_PERIOD = datetime.timedelta(
 SELECTED_SUBSCRIPTIONS = \
     os.environ.get("KCIDB_SELECTED_SUBSCRIPTIONS", "").split()
 
-# A Firestore path to the collection for spooled notifications
-SPOOL_COLLECTION_PATH = os.environ["KCIDB_SPOOL_COLLECTION_PATH"]
-
 # The address of the SMTP host to send notifications through
 SMTP_HOST = os.environ["KCIDB_SMTP_HOST"]
 # The port of the SMTP server to send notifications through
 SMTP_PORT = int(os.environ["KCIDB_SMTP_PORT"])
 # The username to authenticate to SMTP server with
 SMTP_USER = os.environ["KCIDB_SMTP_USER"]
-# The name of the Google Secret Manager's "secret" with SMTP user's password
-SMTP_PASSWORD_SECRET = os.environ["KCIDB_SMTP_PASSWORD_SECRET"]
 # The SMTP user's password
-SMTP_PASSWORD = kcidb.misc.get_secret(PROJECT_ID, SMTP_PASSWORD_SECRET)
+_SMTP_PASSWORD = None
 # The address to tell the SMTP server to send the message to,
 # overriding any recipients in the message itself.
 SMTP_TO_ADDRS = os.environ.get("KCIDB_SMTP_TO_ADDRS", None)
@@ -64,29 +52,105 @@ SMTP_TO_ADDRS = os.environ.get("KCIDB_SMTP_TO_ADDRS", None)
 SMTP_TOPIC = os.environ.get("KCIDB_SMTP_TOPIC", None)
 # The publisher object for email messages, if we're requested to send them to
 # a PubSub topic
-SMTP_PUBLISHER = None if SMTP_TOPIC is None \
-    else kcidb.mq.EmailPublisher(PROJECT_ID, SMTP_TOPIC)
+_SMTP_PUBLISHER = None
 # The name of the subscription for email messages posted to the SMTP topic
 SMTP_SUBSCRIPTION = os.environ.get("KCIDB_SMTP_SUBSCRIPTION", None)
 # The address to use as the "From" address in sent notifications
 SMTP_FROM_ADDR = os.environ.get("KCIDB_SMTP_FROM_ADDR", None)
 # A string to be added to the CC header of notifications being sent out
 EXTRA_CC = os.environ.get("KCIDB_EXTRA_CC", None)
-
 # The database client instance
-DB_CLIENT = kcidb.db.Client(DATABASE)
+_DB_CLIENT = None
 # The object-oriented database client instance
-OO_CLIENT = kcidb.oo.Client(DB_CLIENT)
+_OO_CLIENT = None
 # The notification spool client
-SPOOL_CLIENT = kcidb.monitor.spool.Client(SPOOL_COLLECTION_PATH)
+_SPOOL_CLIENT = None
 # True if the database updates should be published to the updated queue
 UPDATED_PUBLISH = bool(os.environ.get("KCIDB_UPDATED_PUBLISH", ""))
 # The publisher object for the queue with patterns matching objects updated by
 # loading submissions.
-UPDATED_QUEUE_PUBLISHER = kcidb.mq.ORMPatternPublisher(
-    PROJECT_ID,
-    os.environ["KCIDB_UPDATED_QUEUE_TOPIC"]
-)
+_UPDATED_QUEUE_PUBLISHER = None
+
+
+def get_smtp_publisher():
+    """
+    Get the created/cached publisher object for email messages, if we're
+    requested to send them to a PubSub topic, or None, if not.
+    """
+    # It's alright, pylint: disable=global-statement
+    global _SMTP_PUBLISHER
+    if SMTP_TOPIC is not None and _SMTP_PUBLISHER is None:
+        _SMTP_PUBLISHER = kcidb.mq.EmailPublisher(PROJECT_ID, SMTP_TOPIC)
+    return _SMTP_PUBLISHER
+
+
+def get_smtp_password():
+    """Get the (cached) password for the SMTP user"""
+    # It's alright, pylint: disable=global-statement
+    global _SMTP_PASSWORD
+    if _SMTP_PASSWORD is None:
+        secret = os.environ["KCIDB_SMTP_PASSWORD_SECRET"]
+        _SMTP_PASSWORD = kcidb.misc.get_secret(PROJECT_ID, secret)
+    return _SMTP_PASSWORD
+
+
+def get_load_queue_subscriber():
+    """
+    Create or get the cached subscriber object for the submission queue.
+    """
+    # It's alright, pylint: disable=global-statement
+    global _LOAD_QUEUE_SUBSCRIBER
+    if _LOAD_QUEUE_SUBSCRIBER is None:
+        _LOAD_QUEUE_SUBSCRIBER = kcidb.mq.IOSubscriber(
+            PROJECT_ID,
+            os.environ["KCIDB_LOAD_QUEUE_TOPIC"],
+            os.environ["KCIDB_LOAD_QUEUE_SUBSCRIPTION"]
+        )
+    return _LOAD_QUEUE_SUBSCRIBER
+
+
+def get_updated_queue_publisher():
+    """
+    Create or get the cached publisher object for the queue with patterns
+    matching objects updated by loaded submissions. Return None if update
+    publishing is disabled.
+    """
+    # It's alright, pylint: disable=global-statement
+    global _UPDATED_QUEUE_PUBLISHER
+    if UPDATED_PUBLISH and _UPDATED_QUEUE_PUBLISHER is None:
+        _UPDATED_QUEUE_PUBLISHER = kcidb.mq.ORMPatternPublisher(
+            PROJECT_ID,
+            os.environ["KCIDB_UPDATED_QUEUE_TOPIC"]
+        )
+    return _UPDATED_QUEUE_PUBLISHER
+
+
+def get_db_client():
+    """Create or retrieve the cached database client."""
+    # It's alright, pylint: disable=global-statement
+    global _DB_CLIENT
+    if _DB_CLIENT is None:
+        _DB_CLIENT = kcidb.db.Client(os.environ["KCIDB_DATABASE"])
+    return _DB_CLIENT
+
+
+def get_oo_client():
+    """Create or retrieve the cached OO database client."""
+    # It's alright, pylint: disable=global-statement
+    global _OO_CLIENT
+    if _OO_CLIENT is None:
+        _OO_CLIENT = kcidb.oo.Client(get_db_client())
+    return _OO_CLIENT
+
+
+def get_spool_client():
+    """Create or retrieve the cached notification spool client."""
+    # It's alright, pylint: disable=global-statement
+    global _SPOOL_CLIENT
+    if _SPOOL_CLIENT is None:
+        collection_path = os.environ["KCIDB_SPOOL_COLLECTION_PATH"]
+        _SPOOL_CLIENT = kcidb.monitor.spool.Client(collection_path)
+    return _SPOOL_CLIENT
 
 
 # pylint: disable=unused-argument
@@ -100,33 +164,36 @@ def kcidb_load_message(event, context):
     data = kcidb.mq.IOSubscriber.decode_data(base64.b64decode(event["data"]))
     LOGGER.debug("DATA: %s", json.dumps(data))
     # Store it in the database
-    DB_CLIENT.load(data)
-    if UPDATED_PUBLISH:
+    get_db_client().load(data)
+    publisher = get_updated_queue_publisher()
+    if publisher:
         # Generate patterns matching all affected objects
         pattern_set = set()
         for pattern in kcidb.orm.Pattern.from_io(data):
             # TODO Avoid formatting and parsing
             pattern_set |= kcidb.orm.Pattern.parse(repr(pattern) + "<*#")
         # Publish patterns matching all affected objects
-        UPDATED_QUEUE_PUBLISHER.publish(pattern_set)
+        publisher.publish(pattern_set)
 
 
 def kcidb_load_queue(event, context):
     """
-    Load multiple KCIDB data messages from the LOAD_QUEUE_SUBSCRIBER queue
-    into the database, if it stayed unmodified for at least
-    DATABASE_LOAD_PERIOD.
+    Load multiple KCIDB data messages from the load queue into the database,
+    if it stayed unmodified for at least DATABASE_LOAD_PERIOD.
     """
+    subscriber = get_load_queue_subscriber()
+    db_client = get_db_client()
+    publisher = get_updated_queue_publisher()
     # Do nothing, if updated recently
     now = datetime.datetime.now(datetime.timezone.utc)
-    last_modified = DB_CLIENT.get_last_modified()
+    last_modified = db_client.get_last_modified()
     LOGGER.debug("Now: %s, Last modified: %s", now, last_modified)
     if last_modified and now - last_modified < DATABASE_LOAD_PERIOD:
         LOGGER.info("Database too fresh, exiting")
         return
 
     # Pull messages
-    msgs = LOAD_QUEUE_SUBSCRIBER.pull(
+    msgs = subscriber.pull(
         max_num=LOAD_QUEUE_MSG_MAX,
         timeout=LOAD_QUEUE_TIMEOUT_SEC,
         max_obj=LOAD_QUEUE_OBJ_MAX
@@ -148,15 +215,15 @@ def kcidb_load_queue(event, context):
     # Load the merged data into the database
     obj_num = kcidb.io.SCHEMA.count(data)
     LOGGER.debug("Loading %u objects...", obj_num)
-    DB_CLIENT.load(data)
+    db_client.load(data)
     LOGGER.info("Loaded %u objects", obj_num)
 
     # Acknowledge all the loaded messages
     for msg in msgs:
-        LOAD_QUEUE_SUBSCRIBER.ack(msg[0])
+        subscriber.ack(msg[0])
     LOGGER.debug("ACK'ed %u messages", len(msgs))
 
-    if UPDATED_PUBLISH:
+    if publisher:
         # Generate patterns matching all affected objects
         pattern_set = set()
         for pattern in kcidb.orm.Pattern.from_io(data):
@@ -164,7 +231,7 @@ def kcidb_load_queue(event, context):
             pattern_set |= kcidb.orm.Pattern.parse(repr(pattern) + "<*#")
 
         # Publish patterns matching all affected objects
-        UPDATED_QUEUE_PUBLISHER.publish(pattern_set)
+        publisher.publish(pattern_set)
         LOGGER.info("Published updates made by %u loaded objects", obj_num)
 
 
@@ -173,8 +240,10 @@ def kcidb_spool_notifications(event, context):
     Spool notifications about objects matching patterns arriving from a Pub
     Sub subscription
     """
+    oo_client = get_oo_client()
+    spool_client = get_spool_client()
     # Reset the ORM cache
-    OO_CLIENT.reset_cache()
+    oo_client.reset_cache()
     # Get arriving data
     pattern_set = kcidb.mq.ORMPatternSubscriber.decode_data(
         base64.b64decode(event["data"])
@@ -184,11 +253,11 @@ def kcidb_spool_notifications(event, context):
         "".join(repr(p) + "\n" for p in pattern_set)
     )
     # Spool notifications from subscriptions
-    for notification in kcidb.monitor.match(OO_CLIENT.query(pattern_set)):
+    for notification in kcidb.monitor.match(oo_client.query(pattern_set)):
         if not SELECTED_SUBSCRIPTIONS or \
            notification.subscription in SELECTED_SUBSCRIPTIONS:
             LOGGER.info("POSTING %s", notification.id)
-            SPOOL_CLIENT.post(notification)
+            spool_client.post(notification)
         else:
             LOGGER.info("DROPPING %s", notification.id)
 
@@ -197,33 +266,35 @@ def kcidb_send_notification(data, context):
     """
     Send notifications from the spool
     """
+    spool_client = get_spool_client()
     # Get the notification ID
     notification_id = context.resource.split("/")[-1]
     # Pick the notification if we can
-    message = SPOOL_CLIENT.pick(notification_id)
+    message = spool_client.pick(notification_id)
     if not message:
         return
     LOGGER.info("SENDING %s", notification_id)
     # send message via email
     send_message(message)
     # Acknowledge notification as sent
-    SPOOL_CLIENT.ack(notification_id)
+    spool_client.ack(notification_id)
 
 
 def kcidb_pick_notifications(data, context):
     """
     Pick abandoned notifications and send them.
     """
-    for notification_id in SPOOL_CLIENT.unpicked():
+    spool_client = get_spool_client()
+    for notification_id in spool_client.unpicked():
         # Pick abandoned notification and resend
-        message = SPOOL_CLIENT.pick(notification_id)
+        message = spool_client.pick(notification_id)
         if not message:
             continue
         LOGGER.info("SENDING %s", notification_id)
         # send message via email
         send_message(message)
         # Acknowledge notification as sent
-        SPOOL_CLIENT.ack(notification_id)
+        spool_client.ack(notification_id)
 
 
 def send_message(message):
@@ -244,15 +315,16 @@ def send_message(message):
         else:
             message["CC"] = EXTRA_CC
     # If we're requested to divert messages to a PubSub topic
-    if SMTP_PUBLISHER is not None:
-        SMTP_PUBLISHER.publish(message)
+    publisher = get_smtp_publisher()
+    if publisher is not None:
+        publisher.publish(message)
     else:
         # Connect to the SMTP server
         smtp = smtplib.SMTP(host=SMTP_HOST, port=SMTP_PORT)
         smtp.ehlo()
         smtp.starttls()
         smtp.ehlo()
-        smtp.login(SMTP_USER, SMTP_PASSWORD)
+        smtp.login(SMTP_USER, get_smtp_password())
         try:
             # Send message
             smtp.send_message(message, to_addrs=SMTP_TO_ADDRS)
