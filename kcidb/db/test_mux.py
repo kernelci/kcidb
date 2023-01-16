6985af6b080eea94d3f8c6f205931c4c8af6fcd5
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2023-01-16 20:16:22 +0200

    db: mux: Fix support for uninitialized databases

--- kcidb/db/test_mux.py
+++ kcidb/db/test_mux.py
@@ -6,6 +6,7 @@ from kcidb_io.schema import V1_1, V2_0, V3_0, V4_0, V4_1
 from kcidb.unittest import TestCase, local_only
 from kcidb.db.mux import Driver as MuxDriver
 from kcidb.db.null import Driver as NullDriver
+from kcidb.db.misc import UnsupportedSchema
 
 
 class DummyDriver(NullDriver):
@@ -25,7 +26,9 @@ class DummyDriver(NullDriver):
             number of major schema versions per I/O schema, major schema
             version number increment, initial major schema version, initial
             minor schema version, and number of minor versions per major
-            version.
+            version. The initial major schema version can be specified
+            negative to have the emulated database uninitialized (and the
+            initial minor schema version ignored).
 
             Parameters: <MIN_IO_MAJOR>:
                         <MAX_IO_MAJOR>:
@@ -61,14 +64,18 @@ class DummyDriver(NullDriver):
                     (1, 4, 1, 1, 0, 0, 1)
                 )
             )
+        if self.major < 0:
+            self.major = None
+            self.minor = None
         assert min_io_major <= max_io_major
         assert majors_per_io_schema >= 1
         assert self.major_step >= 1
-        assert self.major >= 0
-        assert self.major % self.major_step == 0
-        assert self.minor >= 0
+        assert (self.major is None) == (self.minor is None)
+        assert self.major is None or self.major >= 0
+        assert self.major is None or self.major % self.major_step == 0
+        assert self.major is None or self.minor >= 0
         assert self.minors_per_major >= 1
-        assert self.minor < self.minors_per_major
+        assert self.major is None or self.minor < self.minors_per_major
         io_schemas = filter(
             lambda x: min_io_major <= x.major <= max_io_major,
             V4_1.history
@@ -78,7 +85,7 @@ class DummyDriver(NullDriver):
             for io_schema in io_schemas
             for i in range(0, majors_per_io_schema * self.minors_per_major)
         ]
-        assert \
+        assert self.major is None or \
             0 <= (self.major / self.major_step * self.minors_per_major +
                   self.minor) < len(self.schemas)
         self.params = params
@@ -88,8 +95,8 @@ class DummyDriver(NullDriver):
 
     def get_schemas(self):
         """
-        Retrieve available driver schemas: a dictionary of major version
-        numbers of the driver's schemas (non-negative integers), and
+        Retrieve available driver schemas: a dictionary of major and minor
+        version numbers of the driver's schemas (non-negative integers), and
         corresponding I/O schemas (kcidb_io.schema.abstract.Version instances)
         supported by them.
 
@@ -101,6 +108,28 @@ class DummyDriver(NullDriver):
              index % self.minors_per_major): io_schema
             for index, io_schema in enumerate(self.schemas)
         }
+
+    def is_initialized(self):
+        """
+        Check if the database is initialized.
+
+        Returns:
+            True if the database is initialized, False otherwise.
+        """
+        return self.major is not None
+
+    def init(self, version):
+        """
+        Initialize the driven database. The database must be uninitialized.
+
+        Args:
+            version:    A tuple of the major and minor version numbers (both
+                        non-negative integers) of the schema to initialize the
+                        database to (must be one of the driver's available
+                        schema versions)
+        """
+        assert version in self.get_schemas()
+        (self.major, self.minor) = version
 
     def get_schema(self):
         """
@@ -216,6 +245,87 @@ class MuxDriverTestCase(TestCase):
             (4, 0): V3_0, (5, 0): V3_0, (6, 0): V4_0, (7, 0): V4_0,
             (8, 0): V4_1
         })
+
+    def test_initialized(self):
+        """Check that initialization status is handled correctly"""
+        self.maxDiff = None  # pylint: disable=invalid-name
+
+        # All databases initialized
+        assert DummyMuxDriver("dummy").is_initialized()
+        assert DummyMuxDriver("""
+            dummy
+            dummy
+        """).is_initialized()
+
+        # All databases uninitialized
+        assert not DummyMuxDriver("dummy:1:4:1:1:-1").is_initialized()
+        assert not DummyMuxDriver("""
+            dummy:1:4:1:1:-1
+            dummy:1:4:1:1:-1
+        """).is_initialized()
+
+        # Some databases initialized, some not
+        with self.assertRaises(UnsupportedSchema):
+            DummyMuxDriver("""
+                dummy:1:4:1:1:0
+                dummy:1:4:1:1:-1
+            """)
+
+        # Get schemas of a single uninitialized member driver
+        driver = DummyMuxDriver("""
+            dummy:1:4:1:1:-1
+        """)
+        assert not driver.is_initialized()
+        self.assertEqual(driver.get_schemas(), {
+            (0, 0): V1_1, (1, 0): V2_0, (2, 0): V3_0, (3, 0): V4_0,
+            (4, 0): V4_1,
+        })
+        # Initialize a single-member driver to the first version
+        driver.init((0, 0))
+        assert driver.is_initialized()
+        self.assertEqual(driver.get_schema(), ((0, 0), V1_1))
+
+        # Initialize a single-member driver to the last version
+        driver = DummyMuxDriver("""
+            dummy:1:4:1:1:-1
+        """)
+        driver.init((4, 0))
+        assert driver.is_initialized()
+        self.assertEqual(driver.get_schema(), ((4, 0), V4_1))
+
+        # Get schemas of an uninitialized two-member driver
+        driver = DummyMuxDriver("""
+            dummy:1:4:1:1:-1
+            dummy:1:4:1:1:-1
+        """)
+        assert not driver.is_initialized()
+        self.assertEqual(driver.get_schemas(), {
+            (0, 0): V1_1, (1, 0): V1_1, (2, 0): V2_0, (3, 0): V2_0,
+            (4, 0): V3_0, (5, 0): V3_0, (6, 0): V4_0, (7, 0): V4_0,
+            (8, 0): V4_1,
+        })
+        # Initialize a two-member driver to the first version
+        driver.init((0, 0))
+        assert driver.is_initialized()
+        self.assertEqual(driver.get_schema(), ((0, 0), V1_1))
+
+        # Initialize a two-member driver to the last version
+        driver = DummyMuxDriver("""
+            dummy:1:4:1:1:-1
+            dummy:1:4:1:1:-1
+        """)
+        driver.init((8, 0))
+        assert driver.is_initialized()
+        self.assertEqual(driver.get_schema(), ((8, 0), V4_1))
+
+        # Initialize a two-member driver to a middle version
+        driver = DummyMuxDriver("""
+            dummy:1:4:1:1:-1
+            dummy:1:4:1:1:-1
+        """)
+        driver.init((4, 0))
+        assert driver.is_initialized()
+        self.assertEqual(driver.get_schema(), ((4, 0), V3_0))
 
     def test_schemas(self):  # It's OK, pylint: disable=too-many-branches
         """Check that schemas are enumerated and are upgradable"""
