550c25bacd8f4f96fae71267a0885d8432960dbc
NikkyXO <alexisdame2017@gmail.com>
2023-03-24 10:28:23 +0100

    Revert "initialize database in constructor"

--- kcidb/db/__init__.py
+++ kcidb/db/__init__.py
@@ -74,7 +74,6 @@ class Client(kcidb.orm.Source):
                                   driver
         """
         assert isinstance(database, str)
-        self.database = database
         self.driver = misc.instantiate_spec(DRIVER_TYPES, database)
         assert all(io_schema <= io.SCHEMA
                    for io_schema in self.driver.get_schemas().values()), \
