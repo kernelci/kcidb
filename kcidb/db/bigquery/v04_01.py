f89a33e3778544a38ab1aa717a82aa06d3901d13
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2023-10-09 19:56:15 +0300

    bigquery: Pull schemas from class in _create_table

--- kcidb/db/bigquery/v04_01.py
+++ kcidb/db/bigquery/v04_01.py
@@ -242,6 +242,6 @@ class Schema(PreviousSchema):
         """
         assert isinstance(conn, cls.Connection)
         # Create new tables
-        for table_name, table_schema in cls.TABLE_MAP.items():
+        for table_name in cls.TABLE_MAP:
             if table_name not in PreviousSchema.TABLE_MAP:
-                cls._create_table(conn, table_name, table_schema)
+                cls._create_table(conn, table_name)
