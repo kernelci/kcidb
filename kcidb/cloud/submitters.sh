9c6104ed217bdc0025ce71103c1dcde7b8b8019d
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2024-01-12 21:08:03 +0200

    cloud: Extract submitters.sh

---
+++ kcidb/cloud/submitters.sh
@@ -0,0 +1,31 @@
+# KCIDB cloud deployment - submitters
+#
+if [ -z "${_SUBMITTERS_SH+set}" ]; then
+declare _SUBMITTERS_SH=
+
+. submitter.sh
+. misc.sh
+
+# Deploy submitter permissions
+# Args: project new_topic [submitter...]
+function submitters_deploy() {
+    declare -r project="$1"; shift
+    declare -r new_topic="$1"; shift
+    declare -r -a submitters=("$@")
+    for submitter in "${submitters[@]}"; do
+        submitter_deploy "$project" "$new_topic" "$submitter"
+    done
+}
+
+# Withdraw submitter permissions
+# Args: project new_topic [submitter...]
+function submitters_withdraw() {
+    declare -r project="$1"; shift
+    declare -r new_topic="$1"; shift
+    declare -r -a submitters=("$@")
+    for submitter in "${submitters[@]}"; do
+        submitter_withdraw "$project" "$new_topic" "$submitter"
+    done
+}
+
+fi # _SUBMITTERS_SH
