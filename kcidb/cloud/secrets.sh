bb0f4aa772e5ffb35349e3e62ab9d8d83d90261c
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2024-01-19 13:58:38 +0200

    cloud: Switch to password_secret_ namespace

--- kcidb/cloud/secrets.sh
+++ kcidb/cloud/secrets.sh
@@ -19,7 +19,7 @@ function secrets_deploy() {
     declare exists
 
     # Make sure the shared SMTP password secret is deployed
-    password_deploy_secret smtp
+    password_secret_deploy smtp
     # Give Cloud Functions access to the shared SMTP password secret
     mute gcloud secrets add-iam-policy-binding \
         --quiet --project="$project" "$SECRETS_SMTP_PASSWORD" \
@@ -27,11 +27,11 @@ function secrets_deploy() {
         --member "serviceAccount:$project@appspot.gserviceaccount.com"
 
     # Make sure all PostgreSQL's password secrets are deployed
-    password_deploy_secret psql_superuser psql_editor psql_viewer
+    password_secret_deploy psql_superuser psql_editor psql_viewer
     # DO NOT give Cloud Functions access to *any* PostgreSQL password secrets
 
     # Make sure PostgreSQL's .pgpass secret is deployed
-    password_deploy_pgpass_secret "$project" "$psql_pgpass_secret" \
+    password_secret_deploy_pgpass "$project" "$psql_pgpass_secret" \
         psql_editor "$psql_editor_username"
 
     # Give Cloud Functions access to the .pgpass secret
@@ -47,7 +47,7 @@ function secrets_deploy() {
 function secrets_withdraw() {
     declare -r project="$1"; shift
     declare -r psql_pgpass_secret="$1"; shift
-    password_withdraw_secret psql_editor
+    password_secret_withdraw psql_editor
     secret_withdraw "$project" "$psql_pgpass_secret"
     # NOTE: Not withdrawing the shared secrets
 }
