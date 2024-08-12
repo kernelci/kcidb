41b7fd461a3e8bc0d4f4a796db252434c70d9666
Helen Koike <helen.koike@collabora.com>
2024-08-11 22:42:54 -0300

    DO NOT MERGE: automatching with kcidb-ingest
    Signed-off-by: Helen Koike <helen.koike@collabora.com>

---
+++ kcidb/monitor/subscriptions/create_incidents.py
@@ -0,0 +1,27 @@
+from kcidb.tools import kcidb_match
+import kcidb
+
+
+def match_test(test):
+    kcidb_io_object = {"tests": [test._data],
+                       "builds": [test.build._data],
+                       "checkouts": [test.build.checkout._data]}
+    incident_generator = kcidb_match.IncidentGenerator()
+    incidents = incident_generator.generate_incidents_from_db(kcidb_io_object)
+    if (incidents["incidents"]):
+        kcidb.Client.submit(incidents)
+
+
+def match_build(build):
+    kcidb_io_object = {"builds": [build._data],
+                       "checkouts": [build.checkout._data]}
+    incident_generator = kcidb_match.IncidentGenerator()
+    incidents = incident_generator.generate_incidents_from_db(kcidb_io_object)
+    if (incidents["incidents"]):
+        kcidb.Client.submit(incidents)
+
+
+def match_issues(issues):
+    issue_objects = {"issues": [issues._data]}
+    incident_generator = kcidb_match.IncidentGenerator()
+    incident_generator.db.update_patterns(issue_objects)
